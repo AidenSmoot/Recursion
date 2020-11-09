a8f480bc6848dfb019d0adffb5849be705480aef
AidenSmoot <aiden.p.smoot-1@ou.edu>
2020-11-09 16:19:26 -0600

    Made Cake class

---
+++ src/Cake.java
@@ -0,0 +1,19 @@
+
+public class Cake {
+	private int bites = 10;
+	
+	public Cake() {
+		//nothing to implement
+	}
+	
+	public void takeABite() {
+		--bites;
+		System.out.println("Yummy!");
+	}
+	
+	public boolean noCake() {
+		return bites == 0;
+		//looks like an empty cake
+	}
+	
+}
